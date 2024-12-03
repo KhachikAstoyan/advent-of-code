30f152be7b570adb16d44054b6b6072dd693c6a3
Khachik Astoyan <kastoyan@griddynamics.com>
2024-12-03 18:41:08 +0400

    rewrite day 3 without regex

--- 2024/puzzles/day3/day3.go
+++ 2024/puzzles/day3/day3.go
@@ -2,9 +2,9 @@ package day3
 
 import (
 	_ "embed"
-	"regexp"
-
-	"github.com/KhachikAstoyan/advent-of-code/2024/utils"
+	"errors"
+	"strconv"
+	"strings"
 )
 
 type Day3 struct{}
@@ -13,26 +13,106 @@ type Day3 struct{}
 var input string
 
 func (d Day3) Name() string {
+
 	return "Day 2: Red-Nosed Reports"
 }
 
-var multiplyRegex *regexp.Regexp = regexp.MustCompile(`mul\((\d+),(\d+)\)`)
-
 func (d Day3) Part1() uint64 {
-	matches := multiplyRegex.FindAllStringSubmatch(input, -1)
+	i := 0
 	sum := uint64(0)
 
-	for _, match := range matches {
-		left := utils.ParseInt(match[1], 10, 64)
-		right := utils.ParseInt(match[2], 10, 64)
+outer:
+	for i < len(input)-2 {
+		if input[i:i+3] == "mul" {
+			startIndex := i + 3
+			endIndex := i + 4
 
-		sum += uint64(left) * uint64(right)
+			if input[startIndex] != '(' {
+				i++
+				continue
+			}
+
+			for input[endIndex] != ')' {
+				if isParenthesis(input[endIndex]) {
+					i++
+					continue outer
+				}
+
+				endIndex++
+			}
+			prod, err := multiply(input[startIndex+1 : endIndex])
+			if err != nil {
+				i = endIndex
+				continue
+			}
+
+			sum += prod
+			i = endIndex
+		}
+
+		i++
 	}
 
 	return sum
 }
 
 func (d Day3) Part2() uint64 {
+	i := 0
+	sum := uint64(0)
 
-	return 0
+outer:
+	for i < len(input)-2 {
+		if input[i:i+3] == "mul" {
+			startIndex := i + 3
+			endIndex := i + 4
+
+			if input[startIndex] != '(' {
+				i++
+				continue
+			}
+
+			for input[endIndex] != ')' {
+				if isParenthesis(input[endIndex]) {
+					i++
+					continue outer
+				}
+
+				endIndex++
+			}
+			prod, err := multiply(input[startIndex+1 : endIndex])
+			if err != nil {
+				i = endIndex
+				continue
+			}
+
+			sum += prod
+			i = endIndex
+		}
+
+		i++
+	}
+
+	return sum
+}
+
+func multiply(match string) (uint64, error) {
+	parts := strings.Split(match, ",")
+	if len(parts) != 2 {
+		return 0, errors.New("invalid match")
+	}
+	aStr := strings.TrimSpace(parts[0])
+	bStr := strings.TrimSpace(parts[1])
+
+	a, errA := strconv.ParseInt(aStr, 10, 64)
+	b, errB := strconv.ParseInt(bStr, 10, 64)
+
+	if errA != nil || errB != nil {
+		return 0, errors.New("invalid match")
+	}
+
+	return uint64(a) * uint64(b), nil
+}
+
+func isParenthesis(c byte) bool {
+	return c == '(' || c == ')' || c == '[' || c == ']' || c == '{' || c == '}'
 }
